7d88cb886f90c3c8b24e71141b85a29973283dd9
gigadatagit <gigadatagit@gmail.com>
2025-05-06 14:21:42 -0500

    Cambios en el CRUD de Usuarios

--- pages/paginaEdicionUsuario.py
+++ pages/paginaEdicionUsuario.py
@@ -1,11 +1,4 @@
 import streamlit as st
-import pandas as pd
-import matplotlib.pyplot as plt
-import plotly.express as px
-import io
-import plotly.graph_objects as go
-import requests
-import zipfile
 import login as login
 
 archivo = __file__.split("/")[-1]
