96a43e346b43c32e07e837562dbb2d29feb79608
gigadatagit <gigadatagit@gmail.com>
2025-12-01 15:27:43 -0500

    Se realizan cambios en el contexto para agregar el barrio y ademas se cambian los enlaces de las plantillas

--- pages/paginaCalidadEnergiaBifasico.py
+++ pages/paginaCalidadEnergiaBifasico.py
@@ -308,7 +308,7 @@ if 'correo_electronico' in st.session_state:
                         
                         #var_Enlace_Plantilla = "https://github.com/gigadatagit/GIGA_Data/blob/b7b8b3cb4c88de73ed5db3e843935526b4c17ec3/vars_Template_ETV_Metrel_VATIA_Generada.docx?raw=true"
 
-                        var_Enlace_Plantilla = "https://github.com/gigadatagit/GIGA_Data/blob/f4d019f75b83dcc1a647d5cd71bc14518d5c4f44/plantilla_Word_VATIA_Generada.docx?raw=true"
+                        var_Enlace_Plantilla = "https://github.com/gigadatagit/GIGA_Data/blob/0c25e2bcd5194ec1a9153caebb8c8044ff890094/plantilla_Word_VATIA_Generada.docx?raw=true"
 
                     else:
                         
@@ -316,7 +316,7 @@ if 'correo_electronico' in st.session_state:
 
                         #var_Enlace_Plantilla = "https://github.com/gigadatagit/GIGA_Data/blob/365a61d9e72f3e175c39d5fa6cb1c189e0c70ffa/vars_Template_ETV_Metrel_VATIA5.docx?raw=true"
 
-                        var_Enlace_Plantilla = "https://github.com/gigadatagit/GIGA_Data/blob/71134b28273d4b37fff80fe93acfabdcb4c52177/plantilla_Word_VATIA_NoGenerada.docx?raw=true"
+                        var_Enlace_Plantilla = "https://github.com/gigadatagit/GIGA_Data/blob/88c3e251be32ccc36b4ff2e152107a4e94fa1c47/plantilla_Word_VATIA_NoGenerada.docx?raw=true"
 
                         #print(f"Has elegido no visualizar la información de la Energía Generada {e}")
                         #return  # Salir del menú
@@ -1433,6 +1433,7 @@ if 'correo_electronico' in st.session_state:
                                         'actividadRealizada': str(actividadRealizada).upper(),
                                         'nombreProyecto': str(nombreProyecto).upper(),
                                         'direccionProyecto': str(direccionProyecto).upper(),
+                                        'barrioProyecto': str(barrioProyecto).upper(),
                                         'nombreCiudadoMunicipio': str(nombreCiudadoMunicipio).upper(),
                                         'nombreDepartamento': str(nombreDepartamento).upper(),
                                         'dia': str(dia).upper(),
@@ -2047,6 +2048,7 @@ if 'correo_electronico' in st.session_state:
                                         'actividadRealizada': str(actividadRealizada).upper(),
                                         'nombreProyecto': str(nombreProyecto).upper(),
                                         'direccionProyecto': str(direccionProyecto).upper(),
+                                        'barrioProyecto': str(barrioProyecto).upper(),
                                         'nombreCiudadoMunicipio': str(nombreCiudadoMunicipio).upper(),
                                         'nombreDepartamento': str(nombreDepartamento).upper(),
                                         'dia': str(dia).upper(),
@@ -2661,6 +2663,7 @@ if 'correo_electronico' in st.session_state:
                                         'actividadRealizada': str(actividadRealizada).upper(),
                                         'nombreProyecto': str(nombreProyecto).upper(),
                                         'direccionProyecto': str(direccionProyecto).upper(),
+                                        'barrioProyecto': str(barrioProyecto).upper(),
                                         'nombreCiudadoMunicipio': str(nombreCiudadoMunicipio).upper(),
                                         'nombreDepartamento': str(nombreDepartamento).upper(),
                                         'dia': str(dia).upper(),
@@ -3274,6 +3277,7 @@ if 'correo_electronico' in st.session_state:
                                         'actividadRealizada': str(actividadRealizada).upper(),
                                         'nombreProyecto': str(nombreProyecto).upper(),
                                         'direccionProyecto': str(direccionProyecto).upper(),
+                                        'barrioProyecto': str(barrioProyecto).upper(),
                                         'nombreCiudadoMunicipio': str(nombreCiudadoMunicipio).upper(),
                                         'nombreDepartamento': str(nombreDepartamento).upper(),
                                         'dia': str(dia).upper(),
